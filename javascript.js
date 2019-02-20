5f84a20c0b54be80d5e8bde4653051ab50ad903f
BichDanh1004 <bichdanh1004@gmail.com>
2019-02-20 10:08:13 +0700

    path 3

--- javascript.js
+++ javascript.js
@@ -1,11 +1,9 @@
-function toggle(){
-    var style = document.getElementById('about-contact').style.display;
-    console.log(style);
-    if(style == "none"){
-        style = "block";
-    }
-    else{
-        style = "none";
-    }
-    document.getElementById('about-contact').style.display = style;
-}
+$(document).ready(function() {
+    $("a[href*='#']:not([href='#])").click(function() {
+        let target = $(this).attr("href");
+        $('html,body').stop().animate({
+            scrollTop: $(target).offset().top
+        }, 1000);
+        event.preventDefault();
+    });
+});
